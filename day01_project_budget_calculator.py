812b03466c552ddc65213ec230f4b8946c4444eb
紫晗刘 <zihanliu@zihanliudeMacBook-Air.local>
2025-06-12 13:17:39 +0800

    Refactor:Apply best practices to budget caculator

--- day01_project_budget_calculator.py
+++ day01_project_budget_calculator.py
@@ -1,5 +1,15 @@
+ACCOMMADATION_PRICE = {
+    "东京":800,
+    "大阪":700,
+    "京都":650,
+    "其他":500,
+}
+PLANE_PRICE = 4000
+
+
 # 欢迎语句
 print("欢迎使用日本旅行预算计算器")
+
 
 #询问用户信息
 user_name = input("请输入你的名字")
@@ -9,14 +19,7 @@ user_time = int(user_time)
 user_jrpass = input("是否有日本铁路票？")
 
 #判断住宿费用
-if user_dirction == "东京":
-    living_fine = 800
-elif user_dirction == "大阪":
-    living_fine = 700
-elif user_dirction == "京都":
-    living_fine = 650
-else:
-    living_fine = 500
+living_fine = ACCOMMADATION_PRICE[user_dirction]
 
 #判断交通费用
 if user_jrpass == "是":
@@ -28,16 +31,18 @@ else:
 food_fine = 300
 
 #总花销 机票固定4000元
-total_fine = (food_fine + trans_fine + living_fine)*user_time + 4000
+total_fine = (food_fine + trans_fine + living_fine)*user_time + PLANE_PRICE
 
 #打印用户预算
 #此处出现第二个错误，加号只能连接两个字符串，我这里吧usertime转化为int以方便我运算了，导致我无法使用+
-print("您的名字是 " + user_name)
-print("您的目的地是 " + user_dirction)
-print("您的旅游天数是 " + str(user_time))
+
 print("-------------------")
-print("您的总花销为：" + str(total_fine))
+print(f"尊敬的{user_name},您好！")
+print(f"您的目的地是{user_dirction} ")
+print(f"您的旅游天数是{user_time}")
+print("-------------------")
+print(f"您的总花销为：{total_fine}")
 print("其中：")
-print("住宿费用是：" + str(living_fine) + "每天，*" + str(user_time) + " = " +str(living_fine*user_time))
-print("交通费用是：" + str(trans_fine) + "每天，*" + str(user_time) + " = " +str(trans_fine*user_time))
-print("餐饮费用是：" + str(food_fine) + "每天，*" + str(user_time) + " = " +str(food_fine*user_time))
+print(f"住宿费用是：每天{living_fine}元，{user_time}天共计{user_time*living_fine}元")
+print(f"交通费用是：每天{trans_fine}元，{user_time}天共计{user_time*trans_fine}元")
+print(f"饮食费用是：每天{food_fine}元，{user_time}天共计{user_time*food_fine}元")
